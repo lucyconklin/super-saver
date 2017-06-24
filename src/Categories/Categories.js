64c7a3317ddd09066c360e4257bd621c31f503af
Lucy Conklin <lucy.conklin@gmail.com>
2017-06-24 14:25:14 -0400

    Adds underline in nav, new font

--- src/Categories/Categories.js
+++ src/Categories/Categories.js
@@ -24,6 +24,22 @@ class Categories extends Component {
               <img src='' alt='' />
               <p>Restaurants</p>
             </li>
+            <li>
+              <img src='' alt='' />
+              <p>Entertainment</p>
+            </li>
+            <li>
+              <img src='' alt='' />
+              <p>Pets</p>
+            </li>
+            <li>
+              <img src='' alt='' />
+              <p>Home</p>
+            </li>
+            <li>
+              <img src='' alt='' />
+              <p>Kids</p>
+            </li>
           </ul>
         </div>
       </div>
